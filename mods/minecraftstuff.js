33d27f39e7894225b36e1f3b23e208308da05abe
cybersteve547 <minecraftstevso@gmail.com>
2025-04-11 10:17:59 -0400

    Update and rename minecraftstuff to minecraftstuff.js

---
+++ mods/minecraftstuff.js
@@ -0,0 +1,24 @@
+
+elements.mc_water = {
+	color: "#2965ff",
+	behavior: behaviors.LIQUID,
+	category: "Minecraft",
+	state: "liquid",
+	reactions: {
+		"mc_lava": {elem1: "mc_cobblestone"},
+	}
+};
+
+elements.mc_lava = {
+	color: "#ff9f29",
+	behavior: behaviors.LIQUID,
+	category: "Minecraft",
+	state: "liquid",
+};
+
+elements.mc_cobblestone = {
+	color: "#666666",
+	behavior: behaviors.WALL,
+	category: "Minecraft",
+	state: "solid",
+};
